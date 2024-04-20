212270f8095eb64541ab429e1f99340bfbff87cf
pgredys <gredys.przemyslaw@gmail.com>
2024-04-20 19:05:27 +0200

    better tests

--- src/weather_api.py
+++ src/weather_api.py
@@ -1,10 +1,13 @@
+import inspect
+from pathlib import Path
+
 import requests
 from dotenv import dotenv_values
 
 
 class WeatherAPI:
     def __init__(self):
-        self.API_KEY = dotenv_values(".env").get("API_KEY")
+        self.API_KEY = dotenv_values(Path.joinpath(Path(__file__).parent, ".env")).get("API_KEY")
         self.url_city_name = "https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_key}"
 
     def connection(self):
@@ -16,3 +19,7 @@ class WeatherAPI:
         url = self.url_city_name.format(city_name=city_name, API_key=self.API_KEY)
         response = requests.get(url)
         return response.json()
+
+if __name__ == "__main__":
+    weather_api = WeatherAPI()
+    print(weather_api.get(city_name='London, uk'))
--- tests/test_weather_api.py
+++ tests/test_weather_api.py
@@ -1,17 +1,33 @@
 import unittest
 
+import logging
+
 from weather_api import WeatherAPI
+
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
+logger = logging.getLogger(__file__)
 
 
 class TestWeatherAPI(unittest.TestCase):
 
+    def setUp(self):
+        self.weather_api = WeatherAPI()
+        logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
+        self.logger = logging.getLogger(__file__)
+
     def test_connection(self):
-        weather_api = WeatherAPI()
-        self.assertTrue(weather_api.connection(), 400)
+        logger.info(f'Testing connection to weather api')
+        answer = self.weather_api.connection()
+        self.assertEqual(200, answer)
+        logger.info(f'Connection to weather api code: {answer}\n')
 
     def test_get_city_name(self):
-        weather = WeatherAPI()
-        self.assertIsNotNone(weather.get('London'))
+        logger.info(f'Testing get_city_name method')
+        city = 'London'
+        answer = self.weather_api.get('London')
+        self.assertIsNotNone(answer)
+        self.assertIsNotNone(answer['weather'])
+        logger.info(f'Weather in {city}: {answer["weather"][0]}')
 
 
 if __name__ == '__main__':
