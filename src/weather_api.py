5cd03d703a956be7c854f565e2dbcfc5e37702f3
pgredys <gredys.przemyslaw@gmail.com>
2024-04-20 19:38:55 +0200

    metric units

--- src/weather_api.py
+++ src/weather_api.py
@@ -8,7 +8,8 @@ from dotenv import dotenv_values
 class WeatherAPI:
     def __init__(self):
         self.API_KEY = dotenv_values(Path.joinpath(Path(__file__).parent, ".env")).get("API_KEY")
-        self.url_city_name = "https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_key}"
+        self.url_city_name = ("https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={"
+                              "API_key}&units=metric")
 
     def connection(self):
         url = self.url_city_name.format(city_name='London, uk', API_key=self.API_KEY)
