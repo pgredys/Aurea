1cb2e83a10823a6ea0851999896ea5a752134a08
pgredys <gredys.przemyslaw@gmail.com>
2024-05-06 00:31:32 +0200

    should skip GUI tests for remote testing

--- tests/test_app.py
+++ tests/test_app.py
@@ -11,37 +11,36 @@ logger = logging.getLogger(__file__)
 
 class TestWeatherApp(unittest.TestCase):
 
-    def setUp(self):
-        path = 'src'
-        try:
-            os.chdir(path)
-            print("Current working directory: {0}".format(os.getcwd()))
-        except FileNotFoundError:
-            print("Directory: {0} does not exist".format(path))
-        except NotADirectoryError:
-            print("{0} is not a directory".format(path))
-        except PermissionError:
-            print("You do not have permissions to change to {0}".format(path))
-        self.app = App()
-        logger.info('Weather app started')
-
-    def tearDown(self):
-        self.app.quit()
-        self.app.destroy()
-        logger.info('Weather app destroyed')
-
     def test_app(self, city='London'):
-        title = self.app.winfo_toplevel().title()
-        self.assertEqual(title, 'Aurëa')
-        logger.info('Weather app title correct')
+        if os .environ.get('DISPLAY') is not None:
+            path = 'src'
+            try:
+                os.chdir(path)
+                print("Current working directory: {0}".format(os.getcwd()))
+            except FileNotFoundError:
+                print("Directory: {0} does not exist".format(path))
+            except NotADirectoryError:
+                print("{0} is not a directory".format(path))
+            except PermissionError:
+                print("You do not have permissions to change to {0}".format(path))
+            self.app = App()
+            logger.info('Weather app started')
 
-        self.app.city_entry.insert(0, city)
-        self.app.search_btn_callback()
-        self.assertEqual(self.app.location_lbl._text, city)
-        logger.info('Entry checked')
+            title = self.app.winfo_toplevel().title()
+            self.assertEqual(title, 'Aurëa')
+            logger.info('Weather app title correct')
 
-        self.assertIsInstance(float(self.app.temperature_lbl._text[0:-2]), numbers.Number)
-        logger.info('Basic response checked')
+            self.app.city_entry.insert(0, city)
+            self.app.search_btn_callback()
+            self.assertEqual(self.app.location_lbl._text, city)
+            logger.info('Entry checked')
+
+            self.assertIsInstance(float(self.app.temperature_lbl._text[0:-2]), numbers.Number)
+            logger.info('Basic response checked')
+
+            self.app.quit()
+            self.app.destroy()
+            logger.info('Weather app destroyed')
 
 
 if __name__ == '__main__':
